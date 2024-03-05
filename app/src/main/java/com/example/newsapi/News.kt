3c9b6d363085bc3302aafbf05f5bc00db19d455a
ttaliacc <taliac@bu.edu>
2024-03-05 10:22:56 -0800

    create APis adn create objects for news and articles

--- app/src/main/java/com/example/newsapi/News.kt
+++ app/src/main/java/com/example/newsapi/News.kt
@@ -1,3 +1,26 @@
 package com.example.newsapi
 
-data class News()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class News(
+    val status: String,
+    val totalResults: Int,
+    val articles: List<Article>
+)
+
+data class Source(
+    @PrimaryKey val id: Int,
+    val name: String,
+)
+@Entity
+data class Article(
+    val source: Source,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String,
+    val publishedAt: String,
+    val content: String,
+)
--- app/src/main/java/com/example/newsapi/Retrofit.kt
+++ app/src/main/java/com/example/newsapi/Retrofit.kt
@@ -1,4 +1,16 @@
 package com.example.newsapi
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object Retrofit {
+    private const val BASE_URL = "https://newsapi.org"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
 }
